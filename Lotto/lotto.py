2b8b652035a6c7711067f884b53dbbe64efdaf5c
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-03-25 13:39:26 +0100

    added gitignore and updated lotto.py

--- Lotto/lotto.py
+++ Lotto/lotto.py
@@ -17,30 +17,31 @@ def player_numbers():
 def random_num():
     return set(random.sample(range(1, 50), 6))
 
+if __name__ == '__main__':
 
-counter = 0
-how_many_years = 0
-how_many_4 = 0
-how_many_5 = 0
-numbers = player_numbers()
+    counter = 0
+    how_many_years = 0
+    how_many_4 = 0
+    how_many_5 = 0
+    numbers = player_numbers()
 
-while True:
-    random_numbers = random_num()
-    
-    if random_numbers == numbers:
-        print("Moje liczby: ", numbers)
-        print("Koniec gry, brawo")
-        print(f"Czwórek było {how_many_4} a piątek {how_many_5}.")
-        counter = counter/3
-        how_many_years = counter/52
-        print(f"Zanim wygrałeś mineło, {counter:,.2f} tygodni(a), co daje, {how_many_years:.2f} lat.")
-        break
-    else: 
-        how_many_4or5 = random_numbers.intersection(numbers)
-        if len(how_many_4or5) == 4:
-            how_many_4 += 1
-        if len(how_many_4or5) == 5:
-            how_many_5 += 1
-        counter +=1
-        continue
+    while True:
+        random_numbers = random_num()
+        
+        if random_numbers == numbers:
+            print("Moje liczby: ", numbers)
+            print("Koniec gry, brawo")
+            print(f"Czwórek było {how_many_4} a piątek {how_many_5}.")
+            counter = counter/3
+            how_many_years = counter/52
+            print(f"Zanim wygrałeś mineło, {counter:,.2f} tygodni(a), co daje, {how_many_years:.2f} lat.")
+            break
+        else: 
+            how_many_4or5 = random_numbers.intersection(numbers)
+            if len(how_many_4or5) == 4:
+                how_many_4 += 1
+            if len(how_many_4or5) == 5:
+                how_many_5 += 1
+            counter +=1
+            continue
 
