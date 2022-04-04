ab95a6174ad2a2b35fc79f8c47ffe06a5d76d8e6
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-04-04 21:06:38 +0200

    updated kiler.py, added twitter API

--- twitter/kiler.py
+++ twitter/kiler.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import get
+import tweepy
+import json
 
 URL = 'https://www.telemagazyn.pl/film/kiler-539975/'
 
@@ -7,7 +9,7 @@ page = get(URL)
 bs = BeautifulSoup(page.content, 'html.parser')
 
 
-def download_emission_data(beautifulSoup):
+def is_it_on_tv_tonight(beautifulSoup):
     for emission in beautifulSoup.find_all('div', class_='emisje'):
         emissionDate = emission.find('span', class_='emisjaDzien').get_text()
         emissionTime = emission.find('span', class_='emisjaGodzina').get_text()
@@ -21,4 +23,57 @@ def download_emission_data(beautifulSoup):
 
     return result
 
-print(download_emission_data(bs))
+
+# print(is_it_on_tv_tonight(bs))
+
+
+# def main():
+#     twitter_auth_keys = {
+#         "consumer_key": "xxxxx",
+#         "consumer_secret": "xxxxx",
+#         "access_token": "xxxxx",
+#         "access_token_secret": "xxxxx"
+#     }
+
+#     auth = tweepy.OAuthHandler(
+#         twitter_auth_keys['consumer_key'],
+#         twitter_auth_keys['consumer_secret']
+#     )
+#     auth.set_access_token(
+#         twitter_auth_keys['access_token'],
+#         twitter_auth_keys['access_token_secret']
+#     )
+#     api = tweepy.API(auth)
+
+#     status = "test"
+#     api.update_status(status=status)
+
+# if __name__ == "__main__":
+#     main()
+
+
+def authentication(creds):
+
+    credentials = read_creds(creds)
+    api_key, api_secrets = credentials['api_key'], credentials['api_secrets']
+    token, token_secrets = credentials['access_token'], credentials['access_secret']
+
+    auth = tweepy.OAuthHandler(api_key, api_secrets)
+    auth.set_access_token(token, token_secrets)
+
+    api = tweepy.API(auth)
+
+    status = is_it_on_tv_tonight(bs)
+    api.update_status(status=status)
+
+
+def read_creds(filename):
+
+    with open(filename) as f:
+        credentials = json.load(f)
+    return credentials
+
+
+if __name__ == '__main__':
+    credentials = 'credentials.json'
+    api = authentication(credentials)
