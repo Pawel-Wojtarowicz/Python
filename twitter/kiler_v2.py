145fcb770411c2868d2ce0c243ba32792225ba0b
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-04-07 12:28:05 +0200

    updated kiler ver.2, added quotes.txt

--- twitter/kiler_v2.py
+++ twitter/kiler_v2.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import get
+import codecs
+import random
 
 URL = 'https://www.telemagazyn.pl/film/kiler-539975/'
 
@@ -7,14 +9,23 @@ page = get(URL)
 bs = BeautifulSoup(page.content, 'html.parser')
 
 
+def random_quotes():
+    with codecs.open(r"D:\Python\Git\twitter\quotes.txt", "r", "utf-8") as file:
+        lines = file.read().splitlines()
+        random_quote = random.sample(lines, 1)
+        quote = str("\n".join(random_quote))
+        return quote
+
+
 def is_it_on_tv_tonight(beautifulSoup):
     emissionDays = []
     emissionHours = []
-    emissionChannel = []
+    emissionChannels = []
 
     for day in beautifulSoup.find_all('span', class_='emisjaDzien'):
         result = day.get_text()
-        emissionDays.append(result)
+        day = result.split(" ", 1)
+        emissionDays.append(day[0])
 
     for hours in beautifulSoup.find_all('span', class_='emisjaGodzina'):
         result = hours.get_text()
@@ -23,23 +34,17 @@ def is_it_on_tv_tonight(beautifulSoup):
     for channel in beautifulSoup.find_all('div', class_='emisjaSzczegoly'):
         results = channel.find_all(href=True)
         for result in results:
-            emissionChannel.append(result.get_text())
+            emissionChannels.append(result.get_text())
 
-
-    print("dni:", emissionDays)
-    print("godziny:", emissionHours)
-    print("kanaly: ", emissionChannel)
-    
-"""
-    if (emissionDays[0] != emissionDays[1]):
-        result = emissionDays[0]
-        print(result)
-    elif (emissionDays[0] == emissionDays[1]):
-        day = emissionDays[0].split(" ", 1)
-
-        result = day[0] + emissionHours[0] + emissionHours[1]
-        print(result)
+    if emissionDays[0].startswith("Dzisiaj"):
+        if (emissionDays[0] == emissionDays[2]):
+            return emissionDays[0] + ' o godz:' + emissionHours[0] + ' na kanale ' + emissionChannels[0] + ' oraz o' + emissionHours[1] + ' na kanale ' + emissionChannels[1]
+        elif (emissionDays[0] == "Dzisiaj"):
+            return emissionDays[0] + ' o godzinie' + emissionHours[0] + ' na kanale ' + emissionChannels[0]
     else:
-        print("Dzisiaj nie leci")
-"""
-is_it_on_tv_tonight(bs)
+        return "Dzisiaj nie leci"
+
+emission = is_it_on_tv_tonight(bs)
+quote = random_quotes()
+
+print(emission +"\n"+ "- "+quote)
