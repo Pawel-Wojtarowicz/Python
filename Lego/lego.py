b4c0093b5d176610bdbec4a36c1adcb320147452
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-05-16 15:13:47 +0200

    updated Lego.py

--- Lego/lego.py
+++ Lego/lego.py
@@ -3,29 +3,55 @@ import json
 import pprint
 import webbrowser
 
-# init rebrick module for general reading
-rebrick.init("8e81bf52ab2d71baadb63847e48d5035")
 
-response = rebrick.lego.get_set(input("Enter LEGO number: "))
-data = json.loads(response.read())
+def rebrickInit():
+    # init rebrick module for general reading
+    rebrick.init("8e81bf52ab2d71baadb63847e48d5035")
+    try: 
+        response = rebrick.lego.get_set(input("Enter LEGO number: "))
+    except:
+        print("Nie ma takiego zestawu.")
+        exit(1)
+    data = json.loads(response.read())
+    return data
 
-response = rebrick.lego.get_set_alternates(data['set_num'])
-alternatives = json.loads(response.read())
 
-alternatives_sets = {}
+def get_sets(value):
 
-for key, values in alternatives.items():
-    if key == 'results':
-        for url in values:
-            alternatives_sets[url['moc_img_url']] = url['moc_url']
+    rb = rebrick.Rebrick("8e81bf52ab2d71baadb63847e48d5035", silent=True)
+    result = rb.get_sets(min_pieces=value)
+    for k, v in enumerate(result):
+        print(f"{k+1}. {v}")
+
+
+def getAlternatives(set):
+    response = rebrick.lego.get_set_alternates(set['set_num'])
+    alternatives = json.loads(response.read())
+    return alternatives
+
+
+def alternativesDict(alternatives):
+    alternativesDict = {}
+    for key, values in alternatives.items():
+        if key == 'results':
+            for url in values:
+                alternativesDict[url['moc_img_url']] = url['moc_url']
             # webbrowser.open(url['moc_img_url'])
             # print(url['moc_img_url'])
             # print(url['moc_url'])
+            return alternativesDict
 
-print(f"Alternative sets for {data['name']}:")
-# for key, value in alternatives_sets.items():
-#     print(key, "-", value)
-# print(f"Alternative sets of {response}")
 
-for i, (k, v) in enumerate(alternatives_sets.items()):
-    print(f'{i+1}. {k} - {v}')
+def printAlternativeSets(alternativesDict, data):
+    print(f"Alternative sets for: {data['name']}:")
+    for i, (k, v) in enumerate(alternativesDict.items()):
+        print(f'{i+1}. {k} - {v}')
+
+
+if __name__ == '__main__':
+    
+    # get_sets(value=2000)
+    data = rebrickInit()
+    alternatives = getAlternatives(data)
+    alternativesDict = alternativesDict(alternatives)
+    printAlternativeSets(alternativesDict, data)
